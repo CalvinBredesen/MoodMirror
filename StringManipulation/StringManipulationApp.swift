aaa647436452d9e53191d3bb3ec6507fdcb81181
CalvinBredesen <ctbredesen24@blakeschool.org>
2024-01-26 12:42:04 -0600

    Initial Commit

---
+++ StringManipulation/StringManipulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringManipulationApp.swift
+//  StringManipulation
+//
+//  Created by Calvin Bredesen24 on 1/26/24.
+//
+
+import SwiftUI
+
+@main
+struct StringManipulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
