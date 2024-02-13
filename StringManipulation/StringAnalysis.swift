8405149f9ff999551afdfe934103181c6818fec6
CalvinBredesen <ctbredesen24@blakeschool.org>
2024-02-13 10:37:00 -0600

    First commit

---
+++ StringManipulation/StringAnalysis.swift
@@ -0,0 +1,8 @@
+//
+//  StringAnalysis.swift
+//  StringManipulation
+//
+//  Created by Calvin Bredesen24 on 1/26/24.
+//
+
+import Foundation
---
+++ StringManipulation/Views/HomeScreen.swift
@@ -0,0 +1,18 @@
+//
+//  HomeScreen.swift
+//  StringManipulation
+//
+//  Created by Calvin Bredesen24 on 2/13/24.
+//
+
+import SwiftUI
+
+struct HomeScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeScreen()
+}
