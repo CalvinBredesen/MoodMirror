4fa30f4286c8a1cb7d534b1a70141e152f4af08c
CalvinBredesen <ctbredesen24@blakeschool.org>
2024-02-26 10:33:37 -0600

    Working model, just need to improve UI

---
+++ StringManipulation/Views/ButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  ButtonView.swift
+//  StringManipulation
+//
+//  Created by Calvin Bredesen24 on 2/22/24.
+//
+
+import SwiftUI
+
+struct ButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ButtonView()
+}
