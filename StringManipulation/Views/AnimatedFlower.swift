8135d5f0f68d5ccddf0ddf341ae716a7d25ce6d3
CalvinBredesen <ctbredesen24@blakeschool.org>
2024-02-27 12:18:18 -0600

    Final

---
+++ StringManipulation/Views/AnimatedFlower.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedFlower.swift
+//  StringManipulation
+//
+//  Created by Calvin Bredesen24 on 2/26/24.
+//
+
+import SwiftUI
+
+struct AnimatedFlower: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedFlower()
+}
